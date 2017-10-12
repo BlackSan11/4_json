a33ca8f6cbc03574130d39e5ef70c0b7cad8fc3f
BlackSan11 <bhekpdig@gmail.com>
2017-10-12 18:44:48 +0300

    Исправил названия переменных

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,14 @@
 import json
 
-
 def load_data(filepath):
-    f = open(filepath, "r")
-    filetext = f.read()
-    return filetext
+    file_with_json = open(filepath, "r")
+    json_from_file = file_with_json.read()
+    return json_from_file
 
-def pretty_print_json(data):
-    print(data)
-    res = json.loads(data)
-    print(json.dumps(res, sort_keys=True, indent=4))
+def pretty_print_json(json_for_handling):
+    loaded_json = json.loads(json_for_handling)
+    print(json.dumps(loaded_json, sort_keys=True, indent=4))
 
 if __name__ == '__main__':
-    pyth = input('Enter pyth: ')
-    pretty_print_json(load_data(pyth))
+    path = input('Enter path to JSON file: ')
+    pretty_print_json(load_data(path))
