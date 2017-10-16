3d99d9d2b538de46fb721486bce6eb7d9c4604c6
BlackSan11 <bhekpdig@gmail.com>
2017-10-16 12:06:39 +0300

    Добавил with

--- pprint_json.py
+++ pprint_json.py
@@ -1,9 +1,9 @@
 import json
 import sys
 def load_data(filepath):
-    file_with_json = open(filepath, "r")
-    json_from_file = file_with_json.read()
-    return json_from_file
+    with open(filepath, "r") as file_with_json:
+    	json_from_file = file_with_json.read()
+    	return json_from_file
 def pretty_print_json(json_for_handling):
     loaded_json = json.loads(json_for_handling)
     return json.dumps(loaded_json, sort_keys=True, indent=4)
