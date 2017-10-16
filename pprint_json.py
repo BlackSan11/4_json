b1b8888ff005790054cfb70bd2ffa52d9e61925f
BlackSan11 <bhekpdig@gmail.com>
2017-10-16 14:44:23 +0300

    исправил

--- pprint_json.py
+++ pprint_json.py
@@ -1,11 +1,17 @@
 import json
 import sys
+
+
 def load_data(filepath):
     with open(filepath, "r") as file_with_json:
-    	json_from_file = file_with_json.read()
-    	return json_from_file
-def pretty_print_json(json_for_handling):
-    loaded_json = json.loads(json_for_handling)
-    return json.dumps(loaded_json, sort_keys=True, indent=4)
+        json_from_file = file_with_json.read()
+        loaded_json = json.loads(json_from_file)
+        return loaded_json
+
+
+def reformat_json(json_for_handling):
+    return json.dumps(json_for_handling, sort_keys=True, indent=4)
+
+
 if __name__ == '__main__':
-    print(pretty_print_json(load_data(sys.argv[1])))
+    print(reformat_json(load_data(sys.argv[1])))
