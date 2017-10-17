c4a729c12a0734bf70633b287f156b305375e9f0
BlackSan11 <bhekpdig@gmail.com>
2017-10-17 14:29:28 +0300

    redmi uppd

--- pprint_json.py
+++ pprint_json.py
@@ -4,13 +4,12 @@ import sys
 
 def load_data(filepath):
     with open(filepath, "r") as file_with_json:
-        json_from_file = file_with_json.read()
-        loaded_json = json.loads(json_from_file)
-        return loaded_json
+        json_from_file = json.load(file_with_json)
+        return json_from_file
 
 
 def reformat_json(json_for_handling):
-    return json.dumps(json_for_handling, sort_keys=True, indent=4)
+    return json.dumps(json_for_handling, sort_keys=True, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
